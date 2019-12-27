cf04f034b1b63a6935bc20af42c1b4a051b399ef
Rik Kisnah <rik.kisnah@oracle.com>
2019-12-27 11:15:22 -0800

    Optimize permute code - cleaner

--- recursion/permute.py
+++ recursion/permute.py
@@ -4,19 +4,18 @@ from itertools import permutations
 ## Python program to print all permutations with 
 ## Below are the permutations of string ABC.a
 ##  ABC ACB BAC BCA CBA CAB
-def toString (str):
-  return ''.join(str)
+def permute (s):
+    return _permute(s, 0)
 
-### Ref: https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string
-### Use recursion
-def permute(a, l, r): 
-    if l==r: 
-        print(toString(a))
-    else: 
-        for i in range(l,r+1): 
-            a[l], a[i] = a[i], a[l] 
-            permute(a, l+1, r) 
-            a[l], a[i] = a[i], a[l] # backtrack
+def _permute (s, index):
+    if index==len(s)-1:
+        print("".join(s))
+    else:
+        for i in range (index, len(s)):
+            s[index], s[i] = s[i], s[index]
+            _permute(s, index+1)    
+            # backtrack
+            s[index], s[i] = s[i], s[index] 
 
 #https://www.tutorialspoint.com/How-to-find-all-possible-permutations-of-a-given-string-in-Python
 def permute_inbuilt(s):
@@ -24,12 +23,9 @@ def permute_inbuilt(s):
 		print ("".join(p))
 
 ### Testing ...
-string = "abc"
-length_string = len(string) 
-str_list = list(string)
-
-print("using recursive:")
-permute(str_list, 0, length_string-1)
+str = "abc"
+print ("using recursive approach")
+permute(list(str))
 
 print("using inbuilt itertools permutations:")
-permute_inbuilt(string)
+permute_inbuilt(str)
