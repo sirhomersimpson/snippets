5004b702e890fcd20facff004ee7dba843f83343
Rik Kisnah <rik.kisnah@oracle.com>
2020-04-18 18:54:42 -0700

    Initial commit

---
+++ dropwizard/dwbook-phonebook/src/main/java/com/dwbook/phonebook-DartifactId=dwbook-phonebook/App.java
@@ -0,0 +1,13 @@
+package com.dwbook.phonebook-DartifactId=dwbook-phonebook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
