12127c24e49c0dfe8c71bd34e8ff0bec1e2df861
Rik <rik.kisnah@oracle.com>
2021-01-17 18:36:09 -0800

    Fixed the test dependencies

---
+++ java/dropwizard/DropBookmarks/src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
@@ -0,0 +1,14 @@
+package com.udemy.dropbookmarks.resources;
+import org.junit.*;
+import org.junit.jupiter.api.Assertions;
+
+public class HelloResourceTest {
+
+    @Test
+    public void testGetGreeting() {
+        System.out.println("Testing");
+        Assertions.fail();
+    }
+
+
+}
