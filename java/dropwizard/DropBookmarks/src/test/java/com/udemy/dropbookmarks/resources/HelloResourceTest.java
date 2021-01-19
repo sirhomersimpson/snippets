f188d389de58d9584ac83996e7dfbb2b4efd568f
Rik <rik.kisnah@oracle.com>
2021-01-19 05:32:22 -0800

    Fixed the test dependencies

--- java/dropwizard/DropBookmarks/src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
+++ java/dropwizard/DropBookmarks/src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
@@ -1,13 +1,35 @@
 package com.udemy.dropbookmarks.resources;
+import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.*;
 import org.junit.jupiter.api.Assertions;
 
+import javax.annotation.Resource;
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.core.MediaType;
+
 public class HelloResourceTest {
 
+    @ClassRule
+    public static final ResourceTestRule RULE = ResourceTestRule
+            .builder()
+            .addResource(new HelloResource())
+            .build();
+
+    public HelloResourceTest() {
+
+    }
+
     @Test
-    public void testGetGreeting() {
-        System.out.println("Testing");
-        Assertions.fail();
+     public void testGetGreeting() {
+        String expected = "<b>Hello Rikesh2<b>";
+        String actual = RULE
+                .getJerseyTest()
+                .target("/greeting")
+                .request(MediaType.TEXT_HTML)
+                .get(String.class);
+        System.out.println ("expected: " + expected);
+        System.out.println ("actual: " + actual);
+        Assertions.assertEquals(expected,actual);
     }
 
 
