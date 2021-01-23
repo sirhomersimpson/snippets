9ded172ba587a367697cbca8131b928d104a6587
Rik <rik.kisnah@oracle.com>
2021-01-23 12:47:01 -0800

    Unit tests finally working - ignore assertion

---
+++ java/dropwizard/DropBookmarks/src/test/java/com/udemy/dropbookmarks/resources/HelloResourceTest.java
@@ -0,0 +1,16 @@
+package com.udemy.dropbookmarks.resources;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloResourceTest {
+
+    @Test
+    @Ignore
+    public void getValue() {
+        System.out.println("skipped tests");
+        fail("hello I fail");
+
+    }
+}
